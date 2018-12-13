8da055852309a41869df4f44a4e2879064e19728
gekaWork <absentx@yandex.ru>
2018-12-13 19:11:57 +1100

    Блог админка

--- advanced/frontend/controllers/BlogController.php
+++ advanced/frontend/controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace frontend\controllers;
 
 use common\models\Blog;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 
@@ -10,8 +11,14 @@ class BlogController extends Controller
 {
     public function actionIndex()
     {
-        $blogs = Blog::find()->where(['status_id'=>1])->orderBy('sort')->all();
-        return $this->render('all',['blogs'=>$blogs]);
+        $blogs = Blog::find()->where(['status_id'=>1])->orderBy('sort');
+        $dataProvider = new ActiveDataProvider([
+            'query' => $blogs,
+            'pagination' => [
+                'pageSize' => 2,
+            ],
+        ]);
+        return $this->render('all',['dataProvider'=>$dataProvider]);
     }
 
     public function actionOne($url)
--- advanced/frontend/views/blog/all.php
+++ advanced/frontend/views/blog/all.php
@@ -4,6 +4,7 @@
 /* @var $blogs \common\models\Blog */
 
 use yii\helpers\Html;
+use yii\widgets\ListView;
 
 $this->title = 'blog';
 $this->params['breadcrumbs'][] = $this->title;
@@ -18,19 +19,27 @@ $this->params['breadcrumbs'][] = $this->title;
       <?php // <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>?>
     </div>
 
-    <div class="body-content">
+<!--    <div class="body-content">-->
+<!---->
+<!--        <div class="row">-->
+<!--            --><?php // foreach ($blogs as $one): ?>
+<!--                <div class="col-lg-12">-->
+<!--                    <h2>--><?//=$one->title?><!--</h2>-->
+<!---->
+<!--                    <p>--><?//=$one->text?><!--</p>-->
+<!--                    --><?//= Html::a('подробнее',['blog/one','url'=>$one->url])?>
+<!--                </div>-->
+<!--            --><?php //endforeach;?>
+<!--            -->
+<!--            -->
+<!---->
+<!--        </div>-->
+<!---->
+<!--    </div>-->
+    <?= \yii\widgets\ListView::widget([
+        'dataProvider' => $dataProvider,
+     //   'filterModel' => $searchModel,
+        'itemView' => 'one',
+    ]); ?>
 
-        <div class="row">
-            <?php  foreach ($blogs as $one): ?>
-                <div class="col-lg-12">
-                    <h2><?=$one->title?></h2>
-
-                    <p><?=$one->text?></p>
-                    <?= Html::a('подробнее',['blog/one','url'=>$one->url])?>
-                </div>
-            <?php endforeach;?>
-
-        </div>
-
-    </div>
 </div>
